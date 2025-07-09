dc7b06bb5bdebc384be5fd0bc24466aa4456b2fb
Kishore <kishore.kk067@gmail.com>
2025-07-09 22:00:16 +0530

    feat: implement login and register with backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import { AuthProvider } from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
 import GroupListPage from "./pages/GroupListPage";
 import GroupDetailPage from "./pages/GroupDetailPage";
 import AddExpensePage from "./pages/AddExpensePage";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<HomePage />} />
         <Route path="/groups" element={<GroupListPage />} />
         <Route path="/groups/:id" element={<GroupDetailPage />} />
         <Route path="/add-expense" element={<AddExpensePage />} />
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
